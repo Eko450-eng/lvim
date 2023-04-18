aa6baa7b4d2ff0d2331703ef735eef5e851a2c13
Eko450-eng <ekrem@wipdesign.de>
2023-04-18 14:39:28 +0200

    New config

--- config.lua
+++ config.lua
@@ -5,7 +5,8 @@
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
 vim.opt.relativenumber = true
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "nord"
+
 
 vim.opt.autoindent = true
 vim.opt.background = "dark"
@@ -15,7 +16,10 @@ vim.opt.cmdheight = 1
 
 vim.api.nvim_set_hl(0, "ZenBg", { ctermbg = 0 })
 vim.cmd("autocmd VimEnter * hi ZenBg ctermbg=NONE guibg=NONE")
-vim.cmd("autocmd VimEnter * set scrolloff=999")
+-- Keep line vertically centered
+-- vim.cmd("autocmd VimEnter * set scrolloff=999")
+
+vim.g.editorconfig = true
 
 lvim.builtin.theme.tokyonight = false
 
@@ -24,10 +28,13 @@ lvim.transparent_window = true
 
 vim.g.godot_executable = "/home/eko/appimg/Godot_v4.0-beta8_linux.x86_64"
 
--- Config for postgres
-require("psql").setup({
-  database_name = "dnd",
-})
+--  ╭──────────────────────────────────────────────────────────╮
+--  │ Neovide                                                  │
+--  ╰──────────────────────────────────────────────────────────╯
+vim.g.neovide_transparency = .7
+vim.o.guifont = "Fira Code:h14"
+vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_refresh_rate = 144
 
 --  ╭──────────────────────────────────────────────────────────╮
 --  │ keymappings [view all the defaults by pressing <leader>Lk]│
@@ -42,7 +49,7 @@ lvim.keys.normal_mode["<leader>fs"] = ":w<cr>"
 lvim.keys.normal_mode["<M-l>"] = "<cmd>BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<M-h>"] = "<cmd>BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<leader>bd"] = "<cmd>BufferKill<CR>"
-lvim.keys.normal_mode["<Leader>bD"] = ':%bd!|e #|bd #|normal`"<CR>'
+lvim.keys.normal_mode["<Leader>ba"] = ':%bd!|e #|bd #|normal`"<CR>'
 lvim.keys.normal_mode["<leader>wd"] = ":q<cr>"
 -- lvim.keys.normal_mode["gc"] = "<Plug>(comment_toggle_linewise_current)"
 lvim.keys.normal_mode["<leader>bc"] = ":CBlbox<cr>"
@@ -51,17 +58,6 @@ lvim.keys.normal_mode["<leader>B"] = ":Telescope live_grep <CR>"
 
 lvim.keys.normal_mode["|"] = ":vsplit<cr>"
 lvim.keys.normal_mode["-"] = ":hsplit<cr>"
-
---  ╭──────────────────────────────────────────────────────────╮
---  │ unmap a default keymapping                               │
---  ╰──────────────────────────────────────────────────────────╯
--- vim.keymap.del("n", "<C-Up>")
-
---  ╭──────────────────────────────────────────────────────────╮
---  │ override a default keymapping                            │
---  ╰──────────────────────────────────────────────────────────╯
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
--- lvim.keys.normal_mode["<leader>f"] = nil -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
 --  ╭──────────────────────────────────────────────────────────╮
 --  │ Change Telescope navigation to use j and k for           │
@@ -89,16 +85,9 @@ lvim.builtin.telescope.defaults.mappings = {
 }
 
 --  ╭──────────────────────────────────────────────────────────╮
---  │ Change theme settings                                    │
---  ╰──────────────────────────────────────────────────────────╯
--- vim.builtin.theme.options.style = "night"
-
---  ╭──────────────────────────────────────────────────────────╮
 --  │ Use which-key to add extra bindings with the             │
 --  │  leader-key prefix                                       │
 --  ╰──────────────────────────────────────────────────────────╯
--- lvim.builtin.which_key.mappings["R"] = { "<cmd>Telescope projects<CR>", "Projects" }
---
 
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
@@ -140,65 +129,6 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
-
---  ╭──────────────────────────────────────────────────────────╮
---  │ generic LSP settings                                     │
---  ╰──────────────────────────────────────────────────────────╯
-
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumneko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
 
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
@@ -248,7 +178,6 @@ lvim.plugins = {
   { 'norcalli/snippets.nvim' },
   { 'Shougo/deoplete.nvim' },
   { 'nyoom-engineering/nyoom.nvim' },
-  { 'mzarnitsa/psql' },
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
@@ -258,6 +187,7 @@ lvim.plugins = {
 --  ╭──────────────────────────────────────────────────────────╮
 --  │ Plugin config                                            │
 --  ╰──────────────────────────────────────────────────────────╯
+
 require('nvim-highlight-colors').setup {}
 require('nvim-ts-autotag').setup {}
 require("lspconfig").tsserver.setup {}
@@ -277,11 +207,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
   -- enable wrap mode for json files only
   command = "highlight Comment guifg=lightblue",
 })
-
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
